ec94f34f4c028e7a2f77bb61fb19addd893a3aa0
Gabor Kulcsar <gkulcsar@microsoft.com>
2017-05-25 13:03:57 +0200

    fix 01

--- virtual-machines/n-tier-windows/Deploy-ReferenceArchitecture.ps1
+++ virtual-machines/n-tier-windows/Deploy-ReferenceArchitecture.ps1
@@ -139,7 +139,6 @@ elseif ($Mode -eq "Step3Workload") {
 	$ctx=New-AzureStorageContext -StorageAccountName $fileshareStorageAccountName -StorageAccountKey $storageAccountKey  
 	#Create a new file share
 	$fileshare = New-AzureStorageShare "share1" -Context $ctx
-	$fileshare.Uri
 	
 	Write-Host "Updating web and web2 parameters..."
 	#Get web templates and provide fileShareSettings
